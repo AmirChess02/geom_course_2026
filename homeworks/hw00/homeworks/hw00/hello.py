f0e196d4970f0af68f459a257f1a344efec252d4
Гимазетдинов Амир <a.t.gimazetdinov@gmail.com>
2026-09-14 17:28:03 +0300

    Задание под номеро моего Iq выполнено

---
+++ homeworks/hw00/homeworks/hw00/hello.py
@@ -0,0 +1,8 @@
+﻿import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+print(f"Python version: {sys.version}")
+print(f"NumPy version: {np.__version__}")
+print(f"Pandas version: {pd.__version__}")
+print(f"Matplotlib version: {matplotlib.__version__}")
